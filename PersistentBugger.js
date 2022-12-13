c7130105d76168fb0e86f86356b8b967607416ff
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2022-12-13 23:55:35 +0900

    PersistentBugger

---
+++ PersistentBugger.js
@@ -0,0 +1,13 @@
+function persistence(num) {
+  var count = 0;
+
+  num = num.toString();
+
+  while (num.length > 1) {
+    count++;
+    num = num.split('').map(Number).reduce((a, b) => a*b).toString();
+    
+    }
+
+  return count;
+}
