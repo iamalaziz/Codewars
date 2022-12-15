17d7aa654a9ec5e2940d06f70fe2bf1b6d378ad8
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2022-12-15 22:52:59 +0900

    WeirdStringCase

--- WeirdStringCase.js
+++ WeirdStringCase.js
@@ -2,3 +2,15 @@
 
 The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' '). */
 
+function toWeirdCase(string) {
+  let result = [];
+  const arr = string.split(" ");
+
+  for (let i = 0; i < arr.length; i++) {
+    let word = [...arr[i]].map((l, i) => i % 2 === 0 ? l.toUpperCase() : l.toLowerCase())
+    result.push(word.join(""))
+  }
+  return result.join(" ");
+}
+console.log(toWeirdCase("Hello I am Mike!"));
+// 
